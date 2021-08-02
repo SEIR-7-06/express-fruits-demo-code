c95445adebe43c23703a4e01248e0129cbd6ee36
msolorio <msolorio.github@gmail.com>
2021-08-02 15:28:08 -0700

    converts data to correct form for Create route

--- controllers/fruitsController.js
+++ controllers/fruitsController.js
@@ -31,14 +31,20 @@ router.get('/:fruitIndex', (req, res) => {
 router.post('/', (req, res) => {
   // We need to harvest the data from the form
   console.log(req.body)
+
+  // Convert the data to the correct format
+  if (req.body.readyToEat === 'on') {
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+
   // Add that new fruit data into our database
   fruits.push(req.body)
   res.redirect('/fruits') // redirects to the index route
 })
 
-// Steps to delete a fruit
-// 1. Crete a route to handle deleting the fruit
-// 2. Create a Form to handle the delete request
+// Delete Fruit Route - Delete a fruit from the database
 router.delete('/:fruitIndex', (req, res) => {
   // Logic for deleting fruit
   const fruitIndex = req.params.fruitIndex;
