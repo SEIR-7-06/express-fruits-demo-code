98ea452b9c5820e7656c260c63845f64c290083c
msolorio <msolorio.github@gmail.com>
2021-08-02 15:24:06 -0700

    Adds code to update a fruit

--- controllers/fruitsController.js
+++ controllers/fruitsController.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const fruits = require('../models/fruitModel.js');
 
 
+router.get('/test/:firstName', (req, res) => {
+  console.log(req.params);
+
+  res.send('You hit the test route,')
+})
+
+
 // Index Route - Retrieve many/all fruits
 router.get('/', (req, res) => {
   res.render('index.ejs', { allFruits: fruits });
@@ -23,7 +30,7 @@ router.get('/:fruitIndex', (req, res) => {
 // Create Route - Send data to create a new fruit
 router.post('/', (req, res) => {
   // We need to harvest the data from the form
-  // console.log(req.body)
+  console.log(req.body)
   // Add that new fruit data into our database
   fruits.push(req.body)
   res.redirect('/fruits') // redirects to the index route
@@ -45,12 +52,32 @@ router.delete('/:fruitIndex', (req, res) => {
 // Fruit Edit Route - Serves a form to submit info for updating the fruit
 router.get('/:fruitIndex/edit', (req, res) => {
   res.render('edit.ejs', {
-    oneFruit: fruits[req.params.fruitIndex]
+    oneFruit: fruits[req.params.fruitIndex],
+    index: req.params.fruitIndex
   });
 });
 
 // Create a route to handle a PUT to /fruits/:fruitIndex
 
+// Fruit Update Route - Update the data for a particular fruit
+router.put('/:fruitIndex', (req, res) => {
+  console.log(req.body);
+
+  // 1. Convert the data to the correct format
+  if (req.body.readyToEat === 'on') {
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+
+  console.log(req.body);
+
+  // 2. Update the data in database
+  fruits[req.params.fruitIndex] = req.body;
+
+  // 3. Redirect to the show page for that particular fruit
+  res.redirect('/fruits/' + req.params.fruitIndex);
+})
 
 module.exports = router;
 
