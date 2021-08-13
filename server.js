56dd8fa134981fc59bfac510a2f9f4ec22bf1d1c
msolorio <msolorio.github@gmail.com>
2021-08-13 07:39:43 -0700

    Prepare code for express sessions lesson

--- server.js
+++ server.js
@@ -1,65 +1,54 @@
-/* Keywords */
-// Node - An environment that allows us to run our JavaScript outside of a browser and gives us some helpful server capabilities.
-// Express - A tool / package to help us build our server application.
-// NPM - Node Package Manager
-// package.json - A file that keeps a running list of all of our packages
-// node_modules - A directory that holds all of our packages
-// nodemon - Node Monitor - Listens for when we save our file and restarts the node process (restarts our server)
-
-/* Required Modules*/
+// Require Statements /////////////////////////////////////////////////
 const express = require('express'); // Pulling in the express package into this file
-const rowdy = require('rowdy-logger')
+const rowdy = require('rowdy-logger');
 const methodOverride = require('method-override');
-
-// Database & Models
 const fruitsController = require('./controllers/fruitsController.js');
-/* Variables */
+
+
+// Configuration ///////////////////////////////////////////////////////
 const app = express(); // Creating an instance of an express app
 const port = 4000;
-const rowdyResults = rowdy.begin(app)
-
-/* Middleware */
+const rowdyResults = rowdy.begin(app);
 app.set('view engine', 'ejs');
 
-// Checks if form data was sent in the request.
-// If so adds it to the request body (req.body)
+
+// Middleware //////////////////////////////////////////////////////////
+// Allows us to convert POST requests to PUT and DELETE requests
 app.use(methodOverride('_method'));
-app.use(express.urlencoded({ extended: false })) 
+// Checks if form data was sent in the request and adds to req.body
+app.use(express.urlencoded({ extended: false }));
+
+
+// Controllers /////////////////////////////////////////////////////////
 app.use('/fruits', fruitsController);
 
-app.get('/potato', (req, res) => {
-  res.send('this is not a fruit!');
-})
 
-/* Routes */
+// Routes //////////////////////////////////////////////////////////////
 // Homepage Route
 app.get('/', (req, res) => {
-  // Listen for requests on the '/' route, and when they are received
-  // call this callback function
-  res.send('Welcome to the Fruits App');
+  res.render('home.ejs');
+});
+
+
+// Signup Route - Shows a Signup Form
+app.get('/signup', (req, res) => {
+  res.render('auth/signup.ejs');
+});
+
+
+// Login Route - Shows a Login Form
+app.get('/login', (req, res) => {
+  res.render('auth/login.ejs');
+});
+
+
+app.get('/logout', (req, res) => {
+  res.send('You logged out!');
 })
 
-/* Old routes used for in-class examples  */
-// app.get('/about', (request, response) => {
-//   response.send('All about my website');
-// })
 
-// app.get('/fruits/:fruitIndex/:clientName', (request, response) => {
-//   console.log(fruits[request.params.fruitIndex]);
-
-//   const fruitIndex = request.params.fruitIndex;
-//   const clientName = request.params.clientName;
-
-//   const message = `Hello ${clientName}, here is your ${fruits[fruitIndex]}`
-
-//   response.send(message);
-// });
-
-
-/* Start the Server */
+/* Start the Server */////////////////////////////////////////////////////
 app.listen(port, () => {
-  // Start the server on the specified port
-  // After it starts, call the callback function
   console.log(`Your server is running on port: ${port} 🚀`);
   rowdyResults.print()
 });
